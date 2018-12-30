2943844826607d97227a05f94d186480060d3b0c
muyang-feng <muyang7094@foxmail.com>
2018-12-30 11:46:28 +0800

    练习02

---
+++ xuefeng/practice_02.py
@@ -0,0 +1,28 @@
+# 创建一个函数，除去字符串前面和后面的空格
+# def mean(s):
+#     if len(s) == 0:
+#         return s
+#     elif s[0] == ' ':
+#         return s[1:]
+#     elif s[-1] == ' ':
+#         return s[:-1]
+#     return s
+# print(mean(' 1234567 '))
+
+# 输入一个字符串，判断是否为回文
+# str = input("请输入一个字符串：")
+# def demo(t):
+#     if len(t) < 2:
+#         return True
+#     elif t[0] != t[-1]:
+#         return False
+#     return demo(t[1:-1])
+#
+# if demo(str):
+#     print("它是一个回文字符串！")
+# else:
+#     print("它不是一个回文字符串！")
+
+# 给出一个整型值，能够返回该值得英文，例如输入89，返回eighty-nine，限定值在0~100
+
+
